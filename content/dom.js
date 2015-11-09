bb3d23a7a346573e1e0e8523fa926fae1fe7909b
JustOff <Off.Just.Off@gmail.com>
2015-11-09 11:04:37 +0200

    sstart: get rid of regexp in Dom.child

--- content/dom.js
+++ content/dom.js
@@ -3,15 +3,15 @@ var EXPORTED_SYMBOLS = ["Dom"];
 var Dom = {
 
 	child: function (element, classOrType) {
-		var elements = [ element ];
-		var regexp = new RegExp("\\b" + classOrType + "\\b");
-
-		while ((element = elements.shift())) {
-			for (var i = 0; i < element.childNodes.length; i++) {
-				var child = element.childNodes[i];
-				if (child.nodeName.toLowerCase() == classOrType ||
-					child.className && child.className.match(regexp)) return child;
-				elements.push(child);
+		var i, ch, el, els = [ element ];
+		while ((el = els.shift())) {
+			for (i = 0; i < el.childNodes.length; i++) {
+				ch = el.childNodes[i];
+				if (ch.nodeName.toLowerCase() == classOrType || ch.classList && ch.classList.contains(classOrType)) {
+					return ch;
+				} else {
+					els.push(ch);
+				}
 			}
 		}
 	},
