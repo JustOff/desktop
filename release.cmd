8ba1454f81994bad5870fe587d42d6968b9abac8
JustOff <Off.Just.Off@gmail.com>
2021-02-22 20:20:30 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=2.1.4
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/speed-start-.+?\.xpi/download\/%VER%\/speed-start-%VER%\.xpi/" update.xml
+
+set XPI=speed-start-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
